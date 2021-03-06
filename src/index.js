d3e9b8fccd6c0cb7412df762864f1af03b4ade7c
Dmitrij Ponkin <ponkindmitrij@gmail.com>
2021-03-06 15:55:30 +0300

    Add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+exports.min = function min(array) {
+  if (array === undefined || !array.length) {
+    return 0;
+  }
 
-exports.min = function min (array) {
-  return 0;
-}
+  return Math.min(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (array === undefined || !array.length) {
+    return 0;
+  }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+  return Math.max(...array);
+};
+
+exports.avg = function avg(array) {
+  if (array === undefined || !array.length) {
+    return 0;
+  }
+
+  const result = array.reduce((acc, item) => {
+    const res = acc + item;
+    return res;
+  }, 0);
+  return result / array.length;
+};
